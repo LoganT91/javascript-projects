deed37a3a37d50c2d00c0d958443438a85f68c07
LoganT91 <logan.theiss@outlook.com>
2023-09-21 18:48:01 -0500

    welp got it

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -62,3 +62,12 @@ for (i = 0; i <= 9; i++) {
 }
 console.log(evens);
 console.log(odds);
+
+
+
+
+let phrase = "Chili Cook-off";
+
+for (let i = 0; i < phrase.length - 1; i = i + 3) {
+   console.log(phrase[i]);
+}
--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,7 +1,7 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
 const input = require('readline-sync');
-let startFuelL = 0;
-let astroNum = 0;
+let startFuelL = input.question("what is starting fuel level?");
+let astroNum = input.question('how many astronauts on board?');
 let alt = 0;
 
 
@@ -11,17 +11,23 @@ let alt = 0;
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
 
-  while (startFuelL <= 5000 || startFuelL > 30000 || isNaN(startFuelL)) {
-  startFuelL = input.question('What is the starting fuel level?');
+  while (startFuelL < 5000 || startFuelL > 30000) {
+  console.log(`sorry, ${startFuelL} is not a valid fuel level, please insert a number between 
+  5000 and 30000.`);
+  startFuelL = input.question("what is starting fuel level?");
 }
+console.log('Fuel is good');
 
 
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
- while (astroNum < 0 || astroNum >= 8 || isNaN(astroNum)) { 
-    astroNum = input.question('What is the number of astronauts?');
+ while (astroNum < 1 || astroNum > 7) { 
+    console.log(`sorry, ${astroNum} is not valid number of astronauts. must have between
+    1 and 7.`);
+    astroNum = input.question('how many astronauts on board?');
  }
-  
+  console.log(`Good number of astronauts`);
+
   
   
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
