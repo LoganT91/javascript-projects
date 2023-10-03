eff194993294f20eb7319bcb86b28b00ede2060a
LoganT91 <logan.theiss@outlook.com>
2023-10-02 23:12:34 -0500

    hmm

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -2,7 +2,8 @@ let superChimpOne = {
    name: "Chad",
    species: "Chimpanzee",
    mass: 9,
-   age: 6
+   age: 6,
+   astroID: 1
 };
 
 let salamander = {
@@ -12,7 +13,27 @@ let salamander = {
    age: 5
 };
 
+let chimpanzee = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6
+}
 
+let beagle = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5
+}
+
+let tardigrade = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1
+
+}
 // After you have created the other object literals, add the astronautID property to each one.
 
 // Create an array to hold the animal objects.
