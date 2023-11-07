f3e9c0b432a9d439ea79ac902cd65d88f267e43c
LoganT91 <logan.theiss@outlook.com>
2023-11-06 19:41:32 -0600

    k

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
         response.json().then( function(json) {
-        const div = document.getElementById("weather-conditions");
+        const div = document.getElementById("container");
         });
     });
 });
